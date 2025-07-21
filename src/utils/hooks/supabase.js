11783cf50bba7fa1ce391f923d77b47f3e6c399e
Baker <1bakersharp@gmail.com>
2025-07-21 12:40:55 -0700

    supabase key bug fixed

--- src/utils/hooks/supabase.js
+++ src/utils/hooks/supabase.js
@@ -1,21 +1,17 @@
-import "react-native-url-polyfill/auto";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-import { createClient } from "@supabase/supabase-js";
-
-// Access environment variables from expo-config
-// const { SUPABASE_URL, SUPABASE_ANON_KEY } = Constants.expoConfig?.extra || {};
-// console.log('Supabase URL:', process.env.EXPO_PUBLIC_SUPABASE_URL);
-// console.log('Supabase Anon Key:', process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY);
+import 'react-native-url-polyfill/auto'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient, processLock } from '@supabase/supabase-js'
 
 export const supabase = createClient(
   process.env.EXPO_PUBLIC_SUPABASE_URL,
-  process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
+  process.env.EXPO_PUBLIC_SUPABASE_KEY,
   {
     auth: {
       storage: AsyncStorage,
       autoRefreshToken: true,
       persistSession: true,
       detectSessionInUrl: false,
+      lock: processLock,
     },
-  },
-);
+  })
+  
